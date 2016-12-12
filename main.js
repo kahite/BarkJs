5f2389aa9f08363e8cfb297a2ad9a9a2042450f1
Yves Lelièvre <lelievre.yves@gmail.com>
2016-12-12 22:05:20 +0100

    Add Linux support and execute the right script based on os

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const {app, globalShortcut} = require('electron')
 const exec = require('child_process').exec
+const os = require('os')
+const ostype = os.type()
 
 
 app.on('ready', () => {
-  globalShortcut.register('A', () => {
-    exec('node bark.js')
-  })
+  if(ostype == 'Linux') {
+    globalShortcut.register('A', () => {
+      exec('paplay dog_bark_x.wav')
+    })
+  }
+  else if (ostype.substr(0,7) == 'Windows') {
+    globalShortcut.register('A', () => {
+      exec('node bark.js')
+    })
+  }
 })
 
 app.on('will-quit', () => {
